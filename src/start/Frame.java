a6684c0495795f24532e19541a85d976936a088b
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-06 09:58:41 -0500

    Changed Frame to open full screen

--- src/start/Frame.java
+++ src/start/Frame.java
@@ -1,11 +1,20 @@
 package start;
 
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Frame {
 	static JFrame frame = new JFrame();
+	private int xSize;
+	private int ySize;
+	
 	public Frame(){
 		frame = new JFrame();
+		Toolkit tk = Toolkit.getDefaultToolkit();  
+		xSize = ((int) tk.getScreenSize().getWidth());  
+		ySize = ((int) tk.getScreenSize().getHeight());  
+		frame.setBounds(0,0,xSize,ySize);  
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(null);
 		frame.setVisible(true);
