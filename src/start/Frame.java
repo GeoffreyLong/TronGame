b8c0f4ec0d1ac5538ccbae0de60efd58531c4e34
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-06 09:57:22 -0500

    Made the JFrame for the Frame class

--- src/start/Frame.java
+++ src/start/Frame.java
@@ -1,5 +1,13 @@
 package start;
 
-public class Frame {
+import javax.swing.JFrame;
 
+public class Frame {
+	static JFrame frame = new JFrame();
+	public Frame(){
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
 }
