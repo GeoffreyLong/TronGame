85af75ae3b9927713d195811d3841729d8c0f2cf
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-06 10:03:03 -0500

    Added initial documentation to Main class

--- src/start/Main.java
+++ src/start/Main.java
@@ -1,5 +1,18 @@
+/**
+ * @author Geoffrey Long
+ * 
+ * This class serves to start the program.  The primary function is to 
+ * instantiate the frame.  This is surrounded by a try-catch to ensure 
+ * that no fatal errors arise if the frame cannot be initialized.
+ */
 package start;
 
+/**
+ * The main method will start the program by instantiating the GUI 
+ * via the frame.
+ * @param args
+ * @throws java.lang.Exception if the GUI cannot be instantiated
+ */
 public class Main {
 	static Frame frame;
 	public static void main (String[] args){
