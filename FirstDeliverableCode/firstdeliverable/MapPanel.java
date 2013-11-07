5c81c6057824e8949122aa9596bb63bf8f16ad07
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-07 08:18:43 -0500

    Configured MapPanel to implement KeyListener

--- FirstDeliverableCode/firstdeliverable/MapPanel.java
+++ FirstDeliverableCode/firstdeliverable/MapPanel.java
@@ -1,12 +1,27 @@
 package firstdeliverable;
 
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 
-public class MapPanel extends JPanel {
+public class MapPanel extends JPanel implements KeyListener {
 	@Override
 	public void paintComponents(Graphics g){
 		super.paintComponents(g);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub	
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub	
 	}
 }
