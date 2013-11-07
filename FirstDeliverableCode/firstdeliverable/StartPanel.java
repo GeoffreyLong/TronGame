12939d558acf89cc99f44adc184420bd4ded1af3
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-07 08:40:37 -0500

    Fixed minor formatting errors

--- FirstDeliverableCode/firstdeliverable/StartPanel.java
+++ FirstDeliverableCode/firstdeliverable/StartPanel.java
@@ -13,46 +13,44 @@ public class StartPanel extends JPanel implements ActionListener{
 	private JLabel title;
 	private JLabel description;
 	private JButton start;
-	
+
 	public StartPanel(){
 		makeComponents();
-                makeLayout();
+		makeLayout();
 	}
-	
-	 public Dimension getPreferredSize() {
-                return new Dimension(Frame.getXSize(),Frame.getYSize());
-        }
-	
+
+	public Dimension getPreferredSize() {
+		return new Dimension(Frame.getXSize(),Frame.getYSize());
+	}
+
 	private void makeComponents(){
 		title = new JLabel("Prototype Demonstration I");
-                title.setFont(new Font("Times", Font.BOLD, 37));
-                title.setBounds(310, 10, 510, 45);
-                
-                start = new JButton("Start Game");
-                start.setBounds(440, 166, 200, 60);
-                start.addActionListener(this);
-                
-                description = new JLabel("© Copyright of Group 7, ECSE 321, McGill University");
-                description.setFont(new Font("Times", Font.BOLD, 10));
-                description.setBounds(100, 500, 510, 45);
-		
+		title.setFont(new Font("Times", Font.BOLD, 37));
+		title.setBounds(310, 10, 510, 45);
+
+		start = new JButton("Start Game");
+		start.setBounds(440, 166, 200, 60);
+		start.addActionListener(this);
+
+		description = new JLabel("© Copyright of Group 7, ECSE 321, McGill University");
+		description.setFont(new Font("Times", Font.BOLD, 10));
+		description.setBounds(100, 500, 510, 45);
 	}
-	
+
 	private void makeLayout(){
 		setLayout(null);
-                add(title);
-                add(description);
-                add(start);
+		add(title);
+		add(description);
+		add(start);
 	}
-	
+
 	@Override
-    public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().equals("Start Game")){
 			GameMaster master = new GameMaster();
 			master.gameInit();
 			master.gameStart();
 		}
-    }
-
+	}
 }
 
