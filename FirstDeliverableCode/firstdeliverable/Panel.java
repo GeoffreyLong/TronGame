c2a8abbafcc4600f16de25786febcd9a682e1a18
rishabt <rishabh.tandon@mail.mcgill.ca>
2013-11-06 19:29:32 -0500

    Added the JLabel for description in the Panel

--- FirstDeliverableCode/firstdeliverable/Panel.java
+++ FirstDeliverableCode/firstdeliverable/Panel.java
@@ -11,6 +11,7 @@ import java.awt.event.*;
 public class Panel extends JPanel{
 	
 	private JLabel title;
+	private JLabel description;
 	
 	public Panel(){
 		makeComponents();
@@ -25,12 +26,17 @@ public class Panel extends JPanel{
 		title = new JLabel("Prototype Demonstration I");
                 title.setFont(new Font("Times", Font.BOLD, 37));
                 title.setBounds(310, 10, 510, 45);
+                
+                description = new JLabel("© Copyright of Group 7, ECSE 321, McGill University");
+                description.setFont(new Font("Times", Font.BOLD, 10));
+                description.setBounds(100, 500, 510, 45);
 		
 	}
 	
 	private void makeLayout(){
 		setLayout(null);
                 add(title);
+                add(description);
 	}
 
 }
