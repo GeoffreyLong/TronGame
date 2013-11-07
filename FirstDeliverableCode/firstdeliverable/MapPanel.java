e339a57da13b9f566203bd1ed824d988c05a1ebc
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-07 08:17:22 -0500

    Added skeleton for paintComponents

--- FirstDeliverableCode/firstdeliverable/MapPanel.java
+++ FirstDeliverableCode/firstdeliverable/MapPanel.java
@@ -1,7 +1,12 @@
 package firstdeliverable;
 
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class MapPanel extends JPanel {
-
+	@Override
+	public void paintComponents(Graphics g){
+		super.paintComponents(g);
+	}
 }
