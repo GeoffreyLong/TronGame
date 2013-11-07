3f0f48bba54dbccf17f90010e7599c9df83d0339
rishabt <rishabh.tandon@mail.mcgill.ca>
2013-11-06 19:23:26 -0500

    Completed the Main class that basically runs the program

--- FirstDeliverableCode/firstdeliverable/Main.java
+++ FirstDeliverableCode/firstdeliverable/Main.java
@@ -1,5 +1,13 @@
+/**
+ * @author Rishabh Tandon
+ */
+
 package firstdeliverable;
 
-public class Main {
-
+public class Main{
+        public static void main(String[] args){ 
+                Panel panel = new Panel();
+                Frame frame = new Frame();
+                Frame.addPanel(panel);
+        }
 }
