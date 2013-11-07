44aa7c56f928228d3685983d54801cd56580794e
rishabt <rishabh.tandon@mail.mcgill.ca>
2013-11-06 19:19:11 -0500

    Updated the skeleton code and added another method 'getPreferredSize()' that would be desirable for the implementation

--- FirstDeliverableCode/firstdeliverable/Panel.java
+++ FirstDeliverableCode/firstdeliverable/Panel.java
@@ -11,8 +11,13 @@ import java.awt.event.*;
 public class Panel extends JPanel{
 	
 	public Panel(){
-		
+		makeComponents();
+                makeLayout();
 	}
+	
+	 public Dimension getPreferredSize() {
+                return new Dimension(Frame.getXSize(),Frame.getYSize());
+        }
 	
 	private void makeComponents(){
 		
