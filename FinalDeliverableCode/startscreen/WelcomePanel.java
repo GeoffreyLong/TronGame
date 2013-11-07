807ee8ad5e9fa95e14fa3801aface8f1e338207c
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-07 08:55:45 -0500

    Added a border

--- FinalDeliverableCode/startscreen/WelcomePanel.java
+++ FinalDeliverableCode/startscreen/WelcomePanel.java
@@ -1,11 +1,18 @@
 package startscreen;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import start.Main;
 import firstdeliverable.Frame;
 
 public class WelcomePanel extends JPanel {
 	public WelcomePanel(){
-		setBounds(0,0,Frame.getXSize(),Frame.getYSize()/4);
+		setBounds(0,0,Main.frame.getXSize(),Main.frame.getYSize()/4);
+		setBorder(BorderFactory.createLineBorder(Color.black));
+		setVisible(true);
 	}	
 }
