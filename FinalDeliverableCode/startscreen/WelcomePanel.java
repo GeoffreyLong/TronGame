69cd9d11b9aeddb865fa463fb65e6bfc12e6cdc5
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-07 08:44:28 -0500

    Tentatively set the bounds of the panel

--- FinalDeliverableCode/startscreen/WelcomePanel.java
+++ FinalDeliverableCode/startscreen/WelcomePanel.java
@@ -2,8 +2,10 @@ package startscreen;
 
 import javax.swing.JPanel;
 
+import firstdeliverable.Frame;
+
 public class WelcomePanel extends JPanel {
 	public WelcomePanel(){
-		
-	}
+		setBounds(0,0,Frame.getXSize(),Frame.getYSize()/4);
+	}	
 }
