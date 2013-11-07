d84838aca79466c2db878157b3282a0b92174df1
rishabt <rishabh.tandon@mail.mcgill.ca>
2013-11-06 19:16:37 -0500

    Added the stubs for the methods that still need to be implemented in the class

--- FirstDeliverableCode/firstdeliverable/Frame.java
+++ FirstDeliverableCode/firstdeliverable/Frame.java
@@ -1,5 +1,31 @@
+/**
+ * @author Rishabh Tandon
+ */
+ 
 package firstdeliverable;
 
-public class Frame {
+import java.awt.Toolkit;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Frame{
+	
+	public Frame(){
+		
+	}
+	
+	public static int getXSize(){
+		
+		return 0;
+	}
+	
+	public static int getYSize(){
+		
+		return 0;
+	}
+	
+	public static void addPanel(JPanel panel){
+		
+	}
 }
