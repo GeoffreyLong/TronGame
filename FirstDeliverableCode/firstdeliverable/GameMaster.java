cf1d866980a49530a743a42a7c69c4355b186713
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-07 21:31:37 -0500

    Added a call to Frame.start in gameInit

--- FirstDeliverableCode/firstdeliverable/GameMaster.java
+++ FirstDeliverableCode/firstdeliverable/GameMaster.java
@@ -9,8 +9,11 @@ package firstdeliverable;
 
 public class GameMaster {
 	private Map map;
+	private MapPanel mapPanel;
+	
 	public void gameInit(){
 		map = new Map();
+		mapPanel= Frame.start(map);
 	}		
 	public void gameStart(){
 
