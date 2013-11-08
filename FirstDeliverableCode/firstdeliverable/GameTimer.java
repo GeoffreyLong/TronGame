6e3cc293a41d727b82d887f71faeae1379390d35
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-07 21:34:33 -0500

    Added a constructor to add mapPanel to class

--- FirstDeliverableCode/firstdeliverable/GameTimer.java
+++ FirstDeliverableCode/firstdeliverable/GameTimer.java
@@ -4,6 +4,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GameTimer implements ActionListener {
+	private MapPanel mapPanel;
+	
+	public GameTimer(MapPanel mapPanel) {
+		this.mapPanel = mapPanel;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
