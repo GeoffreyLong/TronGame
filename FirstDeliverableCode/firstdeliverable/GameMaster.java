d5945496a0169210202a82cbe40690f9146ca71d
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-07 21:46:01 -0500

    Added a timer which times out a call to GameTimer

--- FirstDeliverableCode/firstdeliverable/GameMaster.java
+++ FirstDeliverableCode/firstdeliverable/GameMaster.java
@@ -7,16 +7,20 @@
  */
 package firstdeliverable;
 
+import javax.swing.Timer;
+
 public class GameMaster {
 	private Map map;
 	private MapPanel mapPanel;
+	private Timer timer;
 	
 	public void gameInit(){
 		map = new Map();
 		mapPanel= Frame.start(map);
 	}		
 	public void gameStart(){
-
+		timer = new Timer(33, new GameTimer(mapPanel));
+		timer.start();
 	}
 	public static void gameEnd(){
 
