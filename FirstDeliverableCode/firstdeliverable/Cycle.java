a6248b12a2ae897af2001963c80ac0f827ae3ebb
rishabt <rishabh.tandon@mail.mcgill.ca>
2013-11-07 19:33:04 -0500

    Added the skeleton for the constructor for the class

--- FirstDeliverableCode/firstdeliverable/Cycle.java
+++ FirstDeliverableCode/firstdeliverable/Cycle.java
@@ -1,5 +1,15 @@
 package firstdeliverable;
 
 public class Cycle {
+  
+  private int xPos;
+  private int yPos;
+  private int curHeading;
+  private int playerNum;
+  public boolean isAlive;
+  
+  public Cycle(int xPos, int yPos, int curHeading, int playerNum, boolean isAlive){
+    
+  }
 
 }
