19e28a7162e973ad06b27ab8cd5ec928b954baa0
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-07 20:39:19 -0500

    Added a constructor which takes in two Cycle objects

--- FirstDeliverableCode/firstdeliverable/PlayerControl.java
+++ FirstDeliverableCode/firstdeliverable/PlayerControl.java
@@ -1,5 +1,11 @@
 package firstdeliverable;
 
 public class PlayerControl {
-
+	Cycle cycleOne;
+	Cycle cycleTwo;
+	
+	PlayerControl(Cycle cycleOne, Cycle cycleTwo){
+		this.cycleOne = cycleOne;
+		this.cycleTwo = cycleTwo;
+	}
 }
