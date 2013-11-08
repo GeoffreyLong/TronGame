2bf3687d522b44f83b29801f33c9123119eea990
rishabt <rishabh.tandon@mail.mcgill.ca>
2013-11-07 19:43:23 -0500

    Laid out the skeleton code for the class. The method stubs are yet to be implemented

--- FirstDeliverableCode/firstdeliverable/Map.java
+++ FirstDeliverableCode/firstdeliverable/Map.java
@@ -1,5 +1,28 @@
+/**
+ * @author Rishabh Tandon
+ */
+
 package firstdeliverable;
 
 public class Map {
-
+  
+        private int[][] map;
+        private int DEFAULT_MAP_SIZE = 500;
+        private int xSize;
+        private int ySize;
+        
+  
+        public int[][] getMap(){
+          return map;
+        }
+        
+        private void setBorder(int height, int width){
+          
+        }
+        
+        public void setDefaultMap(){
+          
+        }
+  
+  
 }
