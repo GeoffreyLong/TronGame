5ac5126f9a55724481490bd4163125c88fc9d84e
rishabt <rishabh.tandon@mail.mcgill.ca>
2013-11-07 19:35:32 -0500

    Corrected the previous indentation mistakes

--- FirstDeliverableCode/firstdeliverable/Cycle.java
+++ FirstDeliverableCode/firstdeliverable/Cycle.java
@@ -2,18 +2,18 @@ package firstdeliverable;
 
 public class Cycle {
   
-  private int xPos;
-  private int yPos;
-  private int curHeading;
-  private int playerNum;
-  public boolean isAlive;
+        private int xPos;
+        private int yPos;
+        private int curHeading;
+        private int playerNum;
+        public boolean isAlive;
   
   public Cycle(int xPos, int yPos, int curHeading, int playerNum, boolean isAlive){
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.curHeading = curHeading;
-    this.playerNum = playerNum;
-    this.isAlive = isAlive;
+          this.xPos = xPos;
+          this.yPos = yPos;
+          this.curHeading = curHeading;
+          this.playerNum = playerNum;
+          this.isAlive = isAlive;
   }
 
 }
