bee5177871c25f3bc9c24bb0b510183e774f018a
ashley simpson <ashley.simpson@mail.mcgill.ca>
2013-11-10 16:00:23 -0500

    Fixed Frame.java for testing, no tests implemented yet.

--- FirstDeliverableCode/tests/FrameTest.java
+++ FirstDeliverableCode/tests/FrameTest.java
@@ -1,9 +1,15 @@
+/** 
+ * @author Ashley Simpson
+ * @version 2013/11/10
+ * Cycle Testing Class
+ */
+
 package tests;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+import firstdeliverable.Frame;
+
 public class FrameTest {
-	
-	public FrameTest (){
-		
-	}
 	
 }
