340586ba0656a363c18fd89505d45f9a92116f02
ashley simpson <ashley.simpson@mail.mcgill.ca>
2013-11-10 15:47:45 -0500

    Updated Cycle.java getter and setter tests.

--- FirstDeliverableCode/tests/CycleTest.java
+++ FirstDeliverableCode/tests/CycleTest.java
@@ -1,9 +1,43 @@
+/** 
+ * @author Ashley Simpson
+ * @version 2013/11/10
+ * Cycle Testing Class
+ */
+
 package tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import firstdeliverable.Cycle;
+
 public class CycleTest {
+	private int testXPos = 1;
+	private int testYPos = 1;
+	private int testHeading = 1;
+	private int testPlayerNum = 1;
+	private boolean testAliveCase = true;
+	private int testSetXPos = 2;
+	private int testSetYPos = 2;
+	private int testSetHeading = 2;
 	
-	public CycleTest (){
-		
+	@Test
+	public void testCycleGetters() {
+		 Cycle testCycle = new Cycle(testXPos, testYPos, testHeading, testPlayerNum, testAliveCase);
+		 assertEquals(testCycle.getXPos(), testXPos);
+		 assertEquals(testCycle.getYPos(), testYPos);
+		 assertEquals(testCycle.getCurHeading(), testHeading);
+		 assertEquals(testCycle.getPlayerNum(), testPlayerNum);
 	}
 	
+	@Test
+	public void testCycleSetters() {
+		 Cycle testCycle = new Cycle(testXPos, testYPos, testHeading, testPlayerNum, testAliveCase);
+		 testCycle.setXPos(testSetXPos);
+		 assertEquals(testCycle.getXPos(), testSetXPos);
+		 testCycle.setYPos(testSetYPos);
+		 assertEquals(testCycle.getYPos(), testSetYPos);
+		 testCycle.setCurHeading(testSetHeading);
+		 assertEquals(testCycle.getCurHeading(), testSetHeading);
+		 }
 }
