6da54dfc7f2f012e8fec92d23ee9783946f0b400
ashley simpson <ashley.simpson@mail.mcgill.ca>
2013-11-11 10:38:06 -0500

    Fixed some small test incompatibilities

--- Prototype/tests/CycleTest.java
+++ Prototype/tests/CycleTest.java
@@ -8,37 +8,40 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+
 import org.junit.Test;
 
 import prototype.Cycle;
+import prototype.Cycle.Heading;
 
 public class CycleTest {
 	private int testXPos = 1;
 	private int testYPos = 1;
-	private int testHeading = 1;
-	private int testPlayerNum = 1;
+	private Heading testHeading;
 	private boolean testAliveCase = true;
+	private Color testColor;
 	private int testSetXPos = 2;
 	private int testSetYPos = 2;
 	private int testSetHeading = 2;
 	
 	@Test
 	public void testCycleGetters() {
-		 Cycle testCycle = new Cycle(testXPos, testYPos, testHeading, testPlayerNum, testAliveCase);
+		 Cycle testCycle = new Cycle(testXPos, testYPos, testHeading, testAliveCase, testColor);
 		 assertEquals(testCycle.getXPos(), testXPos);
 		 assertEquals(testCycle.getYPos(), testYPos);
 		 assertEquals(testCycle.getCurHeading(), testHeading);
-		 assertEquals(testCycle.getPlayerNum(), testPlayerNum);
+		 //assertEquals(testCycle.getPlayerNum(), testPlayerNum);
 	}
 	
 	@Test
 	public void testCycleSetters() {
-		 Cycle testCycle = new Cycle(testXPos, testYPos, testHeading, testPlayerNum, testAliveCase);
+		 Cycle testCycle = new Cycle(testXPos, testYPos, testHeading, testAliveCase, testColor);
 		 testCycle.setXPos(testSetXPos);
 		 assertEquals(testCycle.getXPos(), testSetXPos);
 		 testCycle.setYPos(testSetYPos);
 		 assertEquals(testCycle.getYPos(), testSetYPos);
-		 testCycle.setCurHeading(testSetHeading);
+		 //testCycle.setCurHeading(testSetHeading);
 		 assertEquals(testCycle.getCurHeading(), testSetHeading);
 		 }
 }
