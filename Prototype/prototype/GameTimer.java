05d1d3c15439f7ac29059db505f672a0db9e7aa2
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-11 09:44:24 -0500

    Add documentation

--- Prototype/prototype/GameTimer.java
+++ Prototype/prototype/GameTimer.java
@@ -1,3 +1,11 @@
+/**
+ * @author Geoffrey Long
+ * 
+ * This class will make a call to MapPanel.updateMap().  
+ * The method call will be done every 33 ms (30 FPS). 
+ * In effect, this class is the game engine.
+ */
+
 package prototype;
 
 import java.awt.event.ActionEvent;
@@ -6,10 +14,20 @@ import java.awt.event.ActionListener;
 public class GameTimer implements ActionListener {
 	private MapPanel mapPanel;
 	
+	/**
+	 * Instantiate the mapPanel as a class variable.  
+	 * Performing this action allows mapPanel to be used every time 
+	 * the timer times out.
+	 * @param mapPanel
+	 */
 	public GameTimer(MapPanel mapPanel) {
 		this.mapPanel = mapPanel;
 	}
 
+	/**
+	 * Calls mapPanel.updateMap() every 33 ms.  
+	 * The method update map paints the map with the new player position.  
+	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		mapPanel.updateMap();
