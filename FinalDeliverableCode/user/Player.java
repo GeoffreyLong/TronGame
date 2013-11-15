a94cc0298523dcc6129ad9aa4cf6f6fb4e05cb68
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-15 05:46:11 -0500

    Add getter and setter for the username

--- FinalDeliverableCode/user/Player.java
+++ FinalDeliverableCode/user/Player.java
@@ -2,11 +2,18 @@ package user;
 
 public class Player {
 	private int playerNumber;
+	private String userName;
 	
 	public Player(int playerNumber){
 		this.playerNumber = playerNumber;
 	}
 	
+	public void setUserName(String userName){
+		this.userName = userName;
+	}	
+	public String getUserName(){
+		return this.userName;
+	}
 	public int getPlayerNumber(){
 		return this.playerNumber;
 	}
