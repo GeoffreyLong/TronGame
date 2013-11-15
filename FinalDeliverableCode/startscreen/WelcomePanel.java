ca4f52b6ea301d23e49f27e44794521947cf0961
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-15 01:52:46 -0500

    Add JLabel
    This JLabel will be the "Welcome to Tron" label.  Still needs to be
    styled.

--- FinalDeliverableCode/startscreen/WelcomePanel.java
+++ FinalDeliverableCode/startscreen/WelcomePanel.java
@@ -13,6 +13,11 @@ public class WelcomePanel extends JPanel {
 	public WelcomePanel(){
 		setBounds(-1,0,Main.frame.getXSize(),Main.frame.getYSize()/4);
 		setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		JLabel welcome = new JLabel("Welcome to Tron");
+		welcome.setBounds(start.Frame.getXCenter()-100, 100, 200, 30);
+		add(welcome);
+		
 		setVisible(true);
 	}	
 }
