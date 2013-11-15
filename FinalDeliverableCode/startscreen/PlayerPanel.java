63e8563af3a4c5d49dd3096f481166361066dbe5
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-15 02:03:37 -0500

    Size both the panels
    We can set the bounds of the PlayerPanels according to the playerNumber
    they were passed with.  This way we can get two appropriately sized
    panels easily.  Again, there is the issue with the sizing of the panel
    relative to the frame.  This will have to be fixed soon.

--- FinalDeliverableCode/startscreen/PlayerPanel.java
+++ FinalDeliverableCode/startscreen/PlayerPanel.java
@@ -3,7 +3,10 @@ package startscreen;
 import java.awt.Color;
 
 import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import start.Frame;
 
 public class PlayerPanel extends JPanel{
 	public PlayerPanel(int playerNum){
@@ -11,6 +14,11 @@ public class PlayerPanel extends JPanel{
 		setLayout(null);
 		setVisible(true);
 		
-		
+		if (playerNum==1){
+			setBounds(-1,Frame.getYSize()/4,Frame.getXSize()/2,3*Frame.getYSize()/4);
+		}
+		else{
+			setBounds(Frame.getXSize()/2-1,Frame.getYSize()/4,Frame.getXSize()/2,3*Frame.getYSize()/4);
+		}
 	}
 }
