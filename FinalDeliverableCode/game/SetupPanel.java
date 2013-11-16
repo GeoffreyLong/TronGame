3f4b11fa69578fa128324da18d9f7bcf98fbff50
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-16 09:57:27 -0500

    Add panel attributes

--- FinalDeliverableCode/game/SetupPanel.java
+++ FinalDeliverableCode/game/SetupPanel.java
@@ -10,10 +10,23 @@ import user.Player;
 public class SetupPanel extends JPanel implements ActionListener{
 	Player playerOne;
 	Player playerTwo;
+	private int xCenter;
+	private int yCenter;
+	private int xSize;
+	private int ySize;
 	
 	public SetupPanel(Player playerOne, Player playerTwo){
 		this.playerOne = playerOne;
 		this.playerTwo = playerTwo;
+		
+		xCenter = start.Frame.getXCenter();
+		yCenter = start.Frame.getYCenter();
+		xSize = start.Frame.getXSize();
+		ySize = start.Frame.getYSize();
+		
+		setLayout(null);
+		setBounds(0,0,xSize,ySize);
+		setVisible(true);
 	}
 
 	@Override
