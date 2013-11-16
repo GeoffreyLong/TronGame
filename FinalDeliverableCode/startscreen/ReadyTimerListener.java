c6d4e6ddc4c8a73c02c7d75e1a041fc0c5ee57d4
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-16 00:18:54 -0500

    Add constructor

--- FinalDeliverableCode/startscreen/ReadyTimerListener.java
+++ FinalDeliverableCode/startscreen/ReadyTimerListener.java
@@ -3,8 +3,17 @@ package startscreen;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ReadyTimerListener implements ActionListener{
+import javax.swing.JLabel;
 
+public class ReadyTimerListener implements ActionListener{
+	private JLabel playerOneReady;
+	private JLabel playerTwoReady;
+	
+    ReadyTimerListener(JLabel playerOneReady, JLabel playerTwoReady){
+    	this.playerOneReady = playerOneReady;
+    	this.playerTwoReady = playerTwoReady;
+    }
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
