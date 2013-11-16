3ff019bf9717512103cd8427e9fc7ed0cebdb4bf
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-16 00:06:53 -0500

    Add playerStatus JLabel as a parameter to the constructor
    This will allow us to make changes on the JLabel via this class.

--- FinalDeliverableCode/startscreen/ReadyAction.java
+++ FinalDeliverableCode/startscreen/ReadyAction.java
@@ -3,9 +3,15 @@ package startscreen;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
+import javax.swing.JLabel;
 
 public class ReadyAction extends AbstractAction{
-
+	private JLabel playerStatus;
+	
+	public ReadyAction(JLabel playerStatus){
+		this.playerStatus = playerStatus;
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
