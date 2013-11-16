a16889f5f2ee2151a572325667aab68c9f6b44f0
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-16 00:12:25 -0500

    Get action and input maps of both panels

--- FinalDeliverableCode/startscreen/ReadyActionListener.java
+++ FinalDeliverableCode/startscreen/ReadyActionListener.java
@@ -1,8 +1,18 @@
 package startscreen;
 
+import java.awt.event.KeyEvent;
+
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+import javax.swing.KeyStroke;
+
 public class ReadyActionListener {
 
 	public ReadyActionListener(PlayerPanel paneOne, PlayerPanel paneTwo){
-		
+		InputMap imOne = paneOne.inputMap;
+		ActionMap amOne = paneOne.getActionMap();
+
+		InputMap imTwo = paneTwo.inputMap;
+		ActionMap amTwo = paneTwo.getActionMap();
 	}
 }
