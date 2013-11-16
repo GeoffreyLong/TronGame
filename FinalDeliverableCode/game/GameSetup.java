77a02e27ebe49afb2a14c3a8398809b7c834d34c
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-16 09:42:58 -0500

    Add game fields

--- FinalDeliverableCode/game/GameSetup.java
+++ FinalDeliverableCode/game/GameSetup.java
@@ -1,10 +1,18 @@
 package game;
 
+import gameplay.Map;
+
+import java.awt.Color;
+
 import user.Player;
 
 public class GameSetup {
 	Player playerOne;
 	Player playerTwo;
+	private int gameDifficulty = 5;
+	private Map map = new Map();
+	private Color colorPOne = Color.RED;
+	private Color colorPTwo = Color.BLUE;
 	
 	public GameSetup(Player playerOne, Player playerTwo){
 		this.playerOne = playerOne;
