75164ab50ed309cc3f52b334ea62d73a2b028f36
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-16 00:14:45 -0500

    Map keystroke to action

--- FinalDeliverableCode/startscreen/ReadyActionListener.java
+++ FinalDeliverableCode/startscreen/ReadyActionListener.java
@@ -6,13 +6,21 @@ import javax.swing.ActionMap;
 import javax.swing.InputMap;
 import javax.swing.KeyStroke;
 
+import startscreen.ReadyAction;
+
 public class ReadyActionListener {
 
 	public ReadyActionListener(PlayerPanel paneOne, PlayerPanel paneTwo){
 		InputMap imOne = paneOne.inputMap;
 		ActionMap amOne = paneOne.getActionMap();
+		
+		imOne.put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0), "w");
+		amOne.put("w", new ReadyAction(paneOne.playerStatus));
 
 		InputMap imTwo = paneTwo.inputMap;
 		ActionMap amTwo = paneTwo.getActionMap();
+		
+		imTwo.put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "up");
+		amTwo.put("up", new ReadyAction(paneTwo.playerStatus));
 	}
 }
