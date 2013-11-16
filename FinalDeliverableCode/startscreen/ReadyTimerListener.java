51bf11051d5c43636519f2d10da5e209b0ce3952
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-16 00:16:36 -0500

    Implement ActionListener

--- FinalDeliverableCode/startscreen/ReadyTimerListener.java
+++ FinalDeliverableCode/startscreen/ReadyTimerListener.java
@@ -1,5 +1,13 @@
 package startscreen;
 
-public class ReadyTimerListener {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class ReadyTimerListener implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
