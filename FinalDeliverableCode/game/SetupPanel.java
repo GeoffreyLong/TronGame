0826881966607ccb155372fd4d9317e263c08a9d
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-16 09:54:54 -0500

    Add class modifiers

--- FinalDeliverableCode/game/SetupPanel.java
+++ FinalDeliverableCode/game/SetupPanel.java
@@ -1,13 +1,24 @@
 package game;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
 import user.Player;
 
-public class SetupPanel {
+public class SetupPanel extends JPanel implements ActionListener{
 	Player playerOne;
 	Player playerTwo;
 	
 	public SetupPanel(Player playerOne, Player playerTwo){
 		this.playerOne = playerOne;
 		this.playerTwo = playerTwo;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
