d61b72549ada748e37054877bdd9370d7e775857
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-16 09:42:16 -0500

    Add constructor

--- FinalDeliverableCode/game/GameSetup.java
+++ FinalDeliverableCode/game/GameSetup.java
@@ -1,5 +1,13 @@
 package game;
 
-public class GameSetup {
+import user.Player;
 
+public class GameSetup {
+	Player playerOne;
+	Player playerTwo;
+	
+	public GameSetup(Player playerOne, Player playerTwo){
+		this.playerOne = playerOne;
+		this.playerTwo = playerTwo;
+	}
 }
