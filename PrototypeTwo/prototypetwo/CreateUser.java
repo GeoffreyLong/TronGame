109d0ede2d8074f17b3f71a2ca01c0505da0374a
rishabt <rishabh.tandon@mail.mcgill.ca>
2013-11-17 17:38:55 -0500

    Made all imports and implemented the ActionListener interface

--- PrototypeTwo/prototypetwo/CreateUser.java
+++ PrototypeTwo/prototypetwo/CreateUser.java
@@ -1,6 +1,19 @@
 package prototypetwo;
 
-public class CreateUser{
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.Connection;
+
+public class CreateUser extends JPanel implements ActionListener{
+  
+  
+  @Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 
 }
