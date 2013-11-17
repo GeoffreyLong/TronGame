9f520317e4633f18fbfc8862e4e025c6b2da5607
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-11-17 17:44:22 -0500

    Added method stubs for CreateUser and Login

--- PrototypeTwo/prototypetwo/DatabaseCalls.java
+++ PrototypeTwo/prototypetwo/DatabaseCalls.java
@@ -4,6 +4,25 @@ import java.sql.*;
 
 public class DatabaseCalls {
 	
+	private Connection conn;
 	
+	public DatabaseCalls(Connection conn){
+		this.conn = conn;
+	}
+	
+	public boolean createUser(String userName, String password){
+
+		return false;
+		
+		
+	}
+	
+	
+	public boolean login(String userName, String password){
+		
+		
+		return false;
+	}
+
 
 }
