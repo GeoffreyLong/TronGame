b2d4048c239c20f108d1c80de0ad3c9168af352d
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-11-17 18:10:14 -0500

    Made the Main method so that the JPanel can be tested at every step to see if the arrangement of JComponents is right

--- PrototypeTwo/prototypetwo/Main2.java
+++ PrototypeTwo/prototypetwo/Main2.java
@@ -4,6 +4,14 @@ import javax.swing.*;
 
 public class Main2 extends JFrame{
 	
+	public static JFrame frame;
 	
+	public static void main(String[] args){
+		frame = new JFrame();
+		WelcomeScreen panel = new WelcomeScreen();
+		frame.add(panel);
+		frame.setBounds(0, 0, 600, 600);
+		frame.setVisible(true);
+	}
 
 }
