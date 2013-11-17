6f14a383330e05a1ddba57ad97ca30d67aec5979
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-11-17 18:06:44 -0500

    Added method stubs for WelcomeScreen and added a constructor. Set layout to null

--- PrototypeTwo/prototypetwo/WelcomeScreen.java
+++ PrototypeTwo/prototypetwo/WelcomeScreen.java
@@ -6,5 +6,31 @@ import java.awt.Font;
 import java.awt.event.*;
 
 public class WelcomeScreen extends JPanel implements ActionListener{
+	
+	
+	public WelcomeScreen(){
+		makeComponents();
+		makeLayout();
+	}
+	
+	private void makeComponents(){
+		
+		
+		
+	}
+	
+	private void makeLayout(){
+		setLayout(null);
+		
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
 
 }
