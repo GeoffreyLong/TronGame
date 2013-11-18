93afaa7f97cac091cfd97f3267090ce184a4bd90
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-18 11:24:34 -0500

    Pass Players as parameter to WelcomePanel

--- PrototypeTwo/prototypetwo/Main2.java
+++ PrototypeTwo/prototypetwo/Main2.java
@@ -6,9 +6,9 @@ public class Main2 extends JFrame{
 	
 	public static void main(String[] args){
 		Frame frame = new Frame();
-		WelcomeScreen panelOne = new WelcomeScreen();
+		WelcomeScreen panelOne = new WelcomeScreen(Players.ONE);
 		panelOne.setBounds(0,0,Frame.getXSize()/2, Frame.getYSize());
-		WelcomeScreen panelTwo = new WelcomeScreen();
+		WelcomeScreen panelTwo = new WelcomeScreen(Players.TWO);
 		panelTwo.setBounds(Frame.getXSize()/2,0,Frame.getXSize()/2, Frame.getYSize());
 		Frame.addPanel(panelOne);
 		Frame.addPanel(panelTwo);
