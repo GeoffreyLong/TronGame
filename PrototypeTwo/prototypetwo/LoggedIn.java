97ce8a1f396a0cb91e46a3fb3c45c30ff0f58d16
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-11-17 23:54:27 -0500

    Add a JButton to go back to the welcome screen

--- PrototypeTwo/prototypetwo/LoggedIn.java
+++ PrototypeTwo/prototypetwo/LoggedIn.java
@@ -1,12 +1,16 @@
 package prototypetwo;
 
+
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class LoggedIn extends JPanel{
+public class LoggedIn extends JPanel implements ActionListener{
 	
 	private JLabel username;
+	private JButton back;
 	
 	public LoggedIn(String username){
 		makeComponents(username);
@@ -19,11 +23,28 @@ public class LoggedIn extends JPanel{
 		this.username.setFont(new Font("Times", Font.BOLD, 30));
 		this.username.setBounds(140, 200, 400, 50);
 		
+		back = new JButton("Back");
+		back.setBounds(20, 500, 80, 30);
+		back.addActionListener(this);
+		
 	}
 	
 	private void makeLayout(){
 		setLayout(null);
 		add(username);
+		add(back);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		if(e.getSource() == back){
+			Main2.frame.getContentPane().removeAll();
+			Main2.frame.getContentPane().add(new WelcomeScreen());
+			Main2.frame.setVisible(true);
+		}
+		
 	}
 
 }
