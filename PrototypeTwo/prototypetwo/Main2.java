1fd15447a30c2626ef47195637ea483c9e3adf1e
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-18 10:01:28 -0500

    Update frame from JFrame to Frame type

--- PrototypeTwo/prototypetwo/Main2.java
+++ PrototypeTwo/prototypetwo/Main2.java
@@ -4,14 +4,10 @@ import javax.swing.*;
 
 public class Main2 extends JFrame{
 	
-	public static JFrame frame;
-	
 	public static void main(String[] args){
-		frame = new JFrame();
+		Frame frame = new Frame();
 		WelcomeScreen panel = new WelcomeScreen();
-		frame.add(panel);
-		frame.setBounds(0, 0, 600, 600);
-		frame.setVisible(true);
+		frame.addPanel(panel);
 	}
 
 }
