8fd9534e300fdcb0567f4e07b56e4e330236e3a6
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-17 22:08:58 -0500

    Add class MapChooser

---
+++ FinalDeliverableCode/game/MapChooser.java
@@ -0,0 +1,12 @@
+package game;
+
+import gameplay.Map;
+
+import javax.swing.JPanel;
+
+public class MapChooser extends JPanel{
+	private Map map;
+	public MapChooser(Map map){
+		this.map = map;
+	}
+}
