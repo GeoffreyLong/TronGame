1f4e179b01c43fe33139f1fbe1185a3205d85258
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-18 12:36:31 -0500

    Add player starting locations

--- FinalDeliverableCode/gameplay/Map.java
+++ FinalDeliverableCode/gameplay/Map.java
@@ -76,6 +76,10 @@ public class Map {
         	//Add one to the sizes for the border
         	private final int X_SIZE = 76;
         	private final int Y_SIZE = 51;
+        	private final int pOneXStart = 1;
+        	private final int pTwoXStart = X_SIZE-1;
+        	private final int pOneYStart = Y_SIZE-1;
+        	private final int pTwoYStart = 1;
         	
         	public MapOne(){
         		setMap();
