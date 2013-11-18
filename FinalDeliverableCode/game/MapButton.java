224ca502131df09f0c2ba71681b416c1824c8063
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-17 22:55:32 -0500

    Add paint method

--- FinalDeliverableCode/game/MapButton.java
+++ FinalDeliverableCode/game/MapButton.java
@@ -1,5 +1,8 @@
 package game;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import gameplay.Map;
 import gameplay.Tile;
 
@@ -13,5 +16,25 @@ public class MapButton extends JButton{
 		this.map = map;
 		setBounds(0,0,110,110);
 		setVisible(true);
+	}
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.setColor(Color.BLACK);
+		for (int i=0; i<100; i+=5){
+			for (int j=0; j<100; j+=5){
+				switch(tiles[i][j]){
+					case WALL:
+						g.setColor(Color.BLACK);
+						break;
+					case EMPTY:
+						g.setColor(Color.WHITE);
+						break;
+					default:
+						break;
+				}
+				g.fillRect(i, j, 1, 1);
+			}
+		}
 	}
 }
