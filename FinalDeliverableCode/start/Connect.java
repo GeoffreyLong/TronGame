de298e5ed6d571ba6b32961f675c24c30a36a970
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-18 14:56:50 -0500

    Replace Connect class with prototypetwo.Connect

--- FinalDeliverableCode/start/Connect.java
+++ FinalDeliverableCode/start/Connect.java
@@ -1,5 +1,46 @@
 package start;
 
-public class Connect {
-
+import java.sql.*;
+ 
+ 
+public class Connect{
+    public static void main(String[] args) {
+          
+    	String url = "jdbc:mysql://ec2-54-201-70-38.us-west-2.compute.amazonaws.com";
+        String dbName = "/authentication";
+        String driver = "com.mysql.jdbc.Driver"; 
+        String userName = "team7";
+        String password = "ecse321";
+        
+        try {
+        	Class.forName(driver).newInstance();
+            Connection conn = DriverManager.getConnection("jdbc:mysql://ec2-54-201-70-225.us-west-2.compute.amazonaws.com:3306/authentication?user=team7&password=ecse321"); 
+            System.out.println("Connected to the Database");
+            conn.close();
+          
+        } catch (Exception e) {
+              e.printStackTrace();
+          }
+   }
+   
+   
+   public static Connection connect(){
+    	
+    	String driver = "com.mysql.jdbc.Driver";
+    	Connection conn = null;
+    	
+    	try{
+    	Class.forName(driver).newInstance();
+     conn = DriverManager.getConnection("jdbc:mysql://ec2-54-201-70-225.us-west-2.compute.amazonaws.com:3306/authentication?user=team7&password=ecse321"); 
+    
+    	}
+    	
+    	catch(Exception e){
+    		
+    	}
+    	
+    	return conn;
+    }
+   
+   
 }
