3c54a68720a89ae4dfa0b5ad819f373b2034b2b1
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-18 10:47:19 -0500

    Launch two welcome panels

--- PrototypeTwo/prototypetwo/Main2.java
+++ PrototypeTwo/prototypetwo/Main2.java
@@ -6,8 +6,12 @@ public class Main2 extends JFrame{
 	
 	public static void main(String[] args){
 		Frame frame = new Frame();
-		WelcomeScreen panel = new WelcomeScreen();
-		frame.addPanel(panel);
+		WelcomeScreen panelOne = new WelcomeScreen();
+		panelOne.setBounds(0,0,Frame.getXSize()/2, Frame.getYSize());
+		WelcomeScreen panelTwo = new WelcomeScreen();
+		panelTwo.setBounds(Frame.getXSize()/2,0,Frame.getXSize()/2, Frame.getYSize());
+		Frame.addPanel(panelOne);
+		Frame.addPanel(panelTwo);
 	}
 
 }
