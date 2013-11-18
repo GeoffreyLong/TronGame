b6151aad9d69fb98354e16ffe39f8b93ce414eda
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-17 22:12:22 -0500

    Add panel initializers

--- FinalDeliverableCode/game/MapChooser.java
+++ FinalDeliverableCode/game/MapChooser.java
@@ -8,5 +8,8 @@ public class MapChooser extends JPanel{
 	private Map map;
 	public MapChooser(Map map){
 		this.map = map;
+		setBounds(0,0,start.Frame.getXSize(), start.Frame.getYSize());
+		setVisible(true);
+		setLayout(null);
 	}
 }
