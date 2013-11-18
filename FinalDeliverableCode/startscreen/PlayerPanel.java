5095aae47810421729c2ea9fc7b986dbe737ede9
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-18 15:34:55 -0500

    Change createUser to createAccount

--- FinalDeliverableCode/startscreen/PlayerPanel.java
+++ FinalDeliverableCode/startscreen/PlayerPanel.java
@@ -49,20 +49,20 @@ public class PlayerPanel extends JPanel implements ActionListener{
 		JLabel playerNameLabel = new JLabel("Logged in as : " + player.getUserName());
 		playerNameLabel.setBounds(140, 325, 300, 30);
 		
-		JButton createUser = new JButton("Create User");
-		createUser.setBounds(340,125,300,70);
-		createUser.addActionListener(this);
-		add(createUser);
+		JButton createAccount = new JButton("Create an Account");
+		createAccount.setBounds(340,125,300,70);
+		createAccount.addActionListener(this);
+		add(createAccount);
 		
 		if (player.getUserName().equals("anonymous")){
 			playerLog = new JButton("Login");
 			playerLog.setBounds(20, 125, 300, 70);
-			createUser.setVisible(true);
+			createAccount.setVisible(true);
 		}
 		else{
 			playerLog = new JButton("Logout");
 			playerLog.setBounds(140, 125, 300, 70);
-			createUser.setVisible(false);
+			createAccount.setVisible(false);
 		}
 		
 		playerLog.addActionListener(this);
