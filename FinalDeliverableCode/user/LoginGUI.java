6bea85e75ff514b18586c56ce2c8acb8f60a8a97
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-18 14:57:37 -0500

    Remove irrelavent panel calls

--- FinalDeliverableCode/user/LoginGUI.java
+++ FinalDeliverableCode/user/LoginGUI.java
@@ -86,15 +86,7 @@ public class LoginGUI extends JPanel implements ActionListener{
 			System.out.println(pass);
 			
 			if(pass){
-				LoggedIn login = new LoggedIn(usernameField.getText(), player);
-				Frame.removePanel(this);
-				if (player == Players.ONE){
-					login.setBounds(0,0,Frame.getXSize()/2, Frame.getYSize());
-				}
-				else{
-					login.setBounds(Frame.getXSize()/2,0,Frame.getXSize()/2, Frame.getYSize());
-				}
-				Frame.addPanel(login);
+
 			}
 			
 			else{
