2034807ba02ab4907d9951e2d09bcdc9c4b1e790
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-18 20:01:04 -0500

    Add the button adder

--- FinalDeliverableCode/game/MapChooser.java
+++ FinalDeliverableCode/game/MapChooser.java
@@ -12,16 +12,30 @@ import javax.swing.JPanel;
 
 public class MapChooser extends JPanel{
 	private Map map;
-	private Tile[][] theMap;
-	public MapChooser(final Map map){
-		this.map = map;
+	private MapHandler mapHandle;
+	public MapChooser(MapHandler mapHandle){
 		setBounds(0,0,start.Frame.getXSize(), start.Frame.getYSize());
+		this.mapHandle = mapHandle;
 		setVisible(true);
 		setLayout(null);
-		
+		setButtons();
+	}
+	private void setButtons(){
+		for (Map map : mapHandle.maps){
+			final Map buttonMap = map;
+			MapButton chooseMap = new MapButton(buttonMap);
+			chooseMap.setBounds(10+100*map.getMapNumber(), 20, 120, 120);
+			chooseMap.addActionListener(new ActionListener(){
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					MapHandler.map = buttonMap;
+				}
+			});
+		}
+	}
+		/*
 		for (int i = 0; i<map.getNumberOfMaps(); i++){
-			MapButton chooseMap = new MapButton(map, i);
-			chooseMap.setBounds(10+100*i, 20, 80, 20);
+
 			chooseMap.addActionListener(new ActionListener(){
 				@Override
 				public void actionPerformed(ActionEvent e) {
@@ -31,8 +45,7 @@ public class MapChooser extends JPanel{
 				}
 			});
 			add(chooseMap);
-		}
-	}
+		}*/
 	
 	@Override
 	public void paintComponent(Graphics g){
