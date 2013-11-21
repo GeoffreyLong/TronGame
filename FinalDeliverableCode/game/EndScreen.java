42af3dbbf5589b761918a4ec5a0d019ed0415417
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-20 19:26:29 -0500

    Add end game status messages

--- FinalDeliverableCode/game/EndScreen.java
+++ FinalDeliverableCode/game/EndScreen.java
@@ -19,6 +19,8 @@ public class EndScreen extends JPanel implements ActionListener{
     private int gamesPlayed;
 	private int pOneWins;
 	private int pTwoWins;
+	private JLabel winner;
+	private JLabel endStatus;
 	
     public EndScreen(int pOneWins, int pTwoWins, int gamesPlayed){
 		this.pOneWins = pOneWins;
@@ -33,6 +35,24 @@ public class EndScreen extends JPanel implements ActionListener{
     	title.setFont(new Font("Times", Font.BOLD, 37));
     	title.setBounds(430, 30, 700, 44);
     	
+    	winner = new JLabel();
+    	if (pOneWins > pTwoWins){
+    		winner.setText("Congradulations Player One");
+    	}
+    	else{
+    		winner.setText("Congradulations Player Two");
+    	}
+    	winner.setBounds(300, 100, 300, 40);
+    	
+    	endStatus = new JLabel("<html>"
+    			+ "Games Played : "
+    			+ gamesPlayed + "<br>"
+    			+ "Player One Wins : "
+    			+ pOneWins + "<br>"
+    			+ "Player Two Wins : "
+    			+ pTwoWins);
+    	endStatus.setBounds(360, 160, 200, 200);
+    	
     	replay = new JButton("Replay?");
     	replay.setBounds(560, 166, 200, 60);
     	replay.addActionListener(this);
@@ -43,7 +63,8 @@ public class EndScreen extends JPanel implements ActionListener{
     	setLayout(null);
     	add(title);
     	add(replay);
-    	
+    	add(endStatus);
+    	add(winner);
     }
     
     public Dimension getPreferredSize() {
