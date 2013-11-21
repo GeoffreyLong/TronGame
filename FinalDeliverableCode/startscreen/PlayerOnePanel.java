bb6763be2d42c91afbdd3901f64e021667c7a8ed
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-21 13:06:03 -0500

    Extend PlayerPanel to PlayerOnePanel and PlayerTwoPanel

---
+++ FinalDeliverableCode/startscreen/PlayerOnePanel.java
@@ -0,0 +1,12 @@
+package startscreen;
+
+import user.Player;
+
+public class PlayerOnePanel extends PlayerPanel{
+
+	public PlayerOnePanel(Player player) {
+		super(player);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ FinalDeliverableCode/startscreen/PlayerTwoPanel.java
@@ -0,0 +1,12 @@
+package startscreen;
+
+import user.Player;
+
+public class PlayerTwoPanel extends PlayerPanel{
+
+	public PlayerTwoPanel(Player player) {
+		super(player);
+		// TODO Auto-generated constructor stub
+	}
+
+}
