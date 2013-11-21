5cf17a5a30792e00e924c3d6a8d5145a4c810be1
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-20 19:05:10 -0500

    Add EndGame constructor and panel initializing skeleton

--- FinalDeliverableCode/game/EndGame.java
+++ FinalDeliverableCode/game/EndGame.java
@@ -1,5 +1,16 @@
 package game;
 
 public class EndGame {
-
+	private int pOneWins;
+	private int pTwoWins;
+	private int gamesPlayed;
+	
+	public EndGame(int pOneWins, int pTwoWins, int gamesPlayed){
+		this.pOneWins = pOneWins;
+		this.pTwoWins = pTwoWins;
+		this.gamesPlayed = gamesPlayed;
+	}
+	public void initPanel(){
+		
+	}
 }
