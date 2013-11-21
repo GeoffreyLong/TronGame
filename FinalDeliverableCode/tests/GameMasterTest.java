c54c8c598be8f883e4d4768e9c1adbac2d89b86c
ashley simpson <ashley.simpson@mail.mcgill.ca>
2013-11-20 23:09:36 -0500

    Partially updated GameMasterTest.java for testing

--- FinalDeliverableCode/tests/GameMasterTest.java
+++ FinalDeliverableCode/tests/GameMasterTest.java
@@ -1,8 +1,29 @@
+/** 
+ * @author Ashley Simpson
+ * @version 2013/11/20
+ * GameMaster Testing Class
+ */
+
 package tests;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+import game.GameSetup;
+import user.Player;
+
 public class GameMasterTest {
+	private int testPlayerOne = 1;
+	private int testPlayerTwo = 2;
+	private Player playerOne = new Player(testPlayerOne);
+	private Player playerTwo = new Player(testPlayerTwo);
+	private GameSetup testGameSetup;
 	
 	public GameMasterTest (){
+		testGameSetup = new GameSetup(playerOne, playerTwo);
+	}
+	
+	@Test
+	public void testGameInit() {
 		
 	}
 	
