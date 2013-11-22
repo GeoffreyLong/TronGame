2c2d2036f9ae26a44c671c8a0295f2f0d0acf743
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-11-21 21:43:31 -0500

    Add call from create user to make an account in statistics for a userName

--- FinalDeliverableCode/start/DatabaseCalls.java
+++ FinalDeliverableCode/start/DatabaseCalls.java
@@ -15,10 +15,15 @@ public class DatabaseCalls {
 		boolean pass = false;
 		
 		String query = "INSERT INTO authentication.login (userName, password) VALUES (\'" + userName + "\',\'" +  password + "\')";
+		String query2 = "INSERT INTO authentication.allStats (userName, totalScore, numberWins, numberLosses) VALUES (\'" + userName + "\', 0, 0, 0";
 		
 		try{
 			Statement stmt = conn.createStatement();
 			stmt.executeUpdate(query);
+			
+			Statement stmt2 = conn.createStatement();
+			stmt2.executeUpdate(query2);
+			
 			pass = true;
 		} 
         
@@ -69,8 +74,8 @@ public class DatabaseCalls {
 		
 		boolean pass = false;
 		
-		String query = "UPDATE authentication.allStatistics SET totalScore = totalScore + " + score1 + "WHERE userName = \'" + userName1 + "\'";
-		String query2 = "UPDATE authentication.allStatistics SET totalScore = totalScore + " + score1 + "WHERE userName = \'" + userName2 + "\'";
+		String query = "UPDATE authentication.allStats SET totalScore = totalScore + " + score1 + "WHERE userName = \'" + userName1 + "\'";
+		String query2 = "UPDATE authentication.allStats SET totalScore = totalScore + " + score1 + "WHERE userName = \'" + userName2 + "\'";
 		
 		try{
 			Statement stmt = conn.createStatement();
