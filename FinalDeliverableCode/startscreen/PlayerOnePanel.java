6032e1d2d4be41cec840be9c3c69b56a5827b67d
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-22 20:16:20 -0500

    Refactor code to pull user specific logic out of PlayerPanel
    Used the NullPlayer class to differentiate between regular players and
    non-logged players.  Used this to place specific items on the
    PlayerPanel such as the title of the panel, and the buttons
    "createAccount", "login", and "logout".  This way the logic is
    controlled by the PlayerOnePanel and PlayerTwoPanel classes and not by
    the PlayerPanel class.

--- FinalDeliverableCode/startscreen/PlayerOnePanel.java
+++ FinalDeliverableCode/startscreen/PlayerOnePanel.java
@@ -3,11 +3,32 @@ package startscreen;
 import user.Player;
 
 public class PlayerOnePanel extends PlayerPanel{
-
+	Player player;
 	public PlayerOnePanel(Player player) {
 		super(player);
+		this.player = player;
+		setLoadout();
+	}
+	public void setLoadout(){
 		setLocation(-1);
 		setLabel("PLAYER ONE");
+		if(player.getClass().getName().equals("user.NullPlayer")){
+			setNull();
+		}
+		else{
+			setUser();
+		}
 	}
-
+	public void setNull(){
+		setLabel("PLAYER ONE");
+		login.setVisible(true);
+		createAccount.setVisible(true);
+		logout.setVisible(false);
+	}
+	public void setUser(){
+		setLabel(player.getUserName());
+		login.setVisible(false);
+		createAccount.setVisible(false);
+		logout.setVisible(true);
+	}
 }
