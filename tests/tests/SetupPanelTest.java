214f95f2bb0112f44bbf883cb85de2859e32a3dd
ashley simpson <ashley.simpson@mail.mcgill.ca>
2013-11-24 13:44:16 -0500

    Updated testChangeColours() in SetupPanelTest.java, tests complete

--- tests/tests/SetupPanelTest.java
+++ tests/tests/SetupPanelTest.java
@@ -17,7 +17,13 @@ import user.Player;
 public class SetupPanelTest {
 	private int playerNumOne = 1;
 	private int playerNumTwo = 2;
-	private Color testColor,BLACK;
+	private SetupPanel.Colors testColour;
+	private SetupPanel.Colors colorRed = SetupPanel.Colors.RED;
+	private SetupPanel.Colors colorBlue = SetupPanel.Colors.BLUE;
+	private SetupPanel.Colors colorYellow = SetupPanel.Colors.YELLOW;
+	private SetupPanel.Colors colorMagenta = SetupPanel.Colors.MAGENTA;
+	private SetupPanel.Colors colorGreen = SetupPanel.Colors.GREEN;
+	private SetupPanel.Colors colorBlack = SetupPanel.Colors.BLACK;
 	
 	@Test
 	public void testChangeColors() {
@@ -27,5 +33,18 @@ public class SetupPanelTest {
 		GameSetup testGameSetup = new GameSetup(testPlayerOne, testPlayerTwo);
 		SetupPanel testSetup = new SetupPanel(testPlayerOne, testPlayerTwo, testGameSetup);
 		
+		// test changing color for default colours
+		testColour = testSetup.colorChange(colorRed);
+		assertEquals(colorYellow, testColour);
+		testColour = testSetup.colorChange(colorBlue);
+		assertEquals(colorYellow, testColour);
+		testColour = testSetup.colorChange(colorYellow);
+		assertEquals(colorMagenta, testColour);
+		testColour = testSetup.colorChange(colorMagenta);
+		assertEquals(colorGreen, testColour);
+		testColour = testSetup.colorChange(colorGreen);
+		assertEquals(colorBlack, testColour);
+		testColour = testSetup.colorChange(colorBlack);
+		assertEquals(colorYellow, testColour);
 	}
 }
