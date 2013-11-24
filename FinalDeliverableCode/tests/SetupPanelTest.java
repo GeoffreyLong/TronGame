bef3656766cf50c7ba3636ad2baf0ef11b01fbf7
ashley simpson <ashley.simpson@mail.mcgill.ca>
2013-11-23 20:16:13 -0500

    Created SetupPanelTest.java

---
+++ FinalDeliverableCode/tests/SetupPanelTest.java
@@ -0,0 +1,17 @@
+/** 
+ * @author Ashley Simpson
+ * @version 2013/11/23
+ * Setup Panel Testing Class
+ */
+
+package tests;
+
+import static org.junit.Assert.*;
+import java.awt.Color;
+import org.junit.Test;
+
+import game.SetupPanel;
+
+public class SetupPanelTest {
+	
+}
