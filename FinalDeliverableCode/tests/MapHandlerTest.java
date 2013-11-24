505e1449e342bfd5e9419b4e32469700758a700e
ashley simpson <ashley.simpson@mail.mcgill.ca>
2013-11-23 19:15:26 -0500

    Push MapHandlerTest.java

---
+++ FinalDeliverableCode/tests/MapHandlerTest.java
@@ -0,0 +1,16 @@
+/** 
+ * @author Ashley Simpson
+ * @version 2013/11/14
+ * MapHandler Testing Class
+ */
+
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import game.Map;
+
+public class MapHandlerTest {
+	
+}
