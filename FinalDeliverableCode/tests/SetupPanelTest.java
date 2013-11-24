f76b024f8ee51696c30f89ab2848f4d536782612
ashley simpson <ashley.simpson@mail.mcgill.ca>
2013-11-23 20:28:17 -0500

    Created testChangeColors() in SetupPanelTest.java

--- FinalDeliverableCode/tests/SetupPanelTest.java
+++ FinalDeliverableCode/tests/SetupPanelTest.java
@@ -11,7 +11,21 @@ import java.awt.Color;
 import org.junit.Test;
 
 import game.SetupPanel;
+import game.GameSetup;
+import user.Player;
 
 public class SetupPanelTest {
+	private int playerNumOne = 1;
+	private int playerNumTwo = 2;
+	private Color testColor,BLACK;
 	
+	@Test
+	public void testChangeColors() {
+		// create necessary objects
+		Player testPlayerOne = new Player(playerNumOne);
+		Player testPlayerTwo = new Player(playerNumTwo);
+		GameSetup testGameSetup = new GameSetup(testPlayerOne, testPlayerTwo);
+		SetupPanel testSetup = new SetupPanel(testPlayerOne, testPlayerTwo, testGameSetup);
+		
+	}
 }
