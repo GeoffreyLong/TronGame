9c5ac8d4551b68b7beaf57610bfeee6b2e98b8ba
ashley simpson <ashley.simpson@mail.mcgill.ca>
2013-11-25 01:51:08 -0500

    Created CreateUserTest.java for testing

---
+++ tests/tests/CreateUserTest.java
@@ -0,0 +1,17 @@
+/** 
+ * @author Ashley Simpson
+ * @version 2013/11/14
+ * CreateUser Testing Class
+ */
+
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import user.CreateUser;
+import user.Player;
+
+public class CreateUserTest {
+	
+}
