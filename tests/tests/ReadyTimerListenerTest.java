5af8d1c2c20f43926f16888803f5da328d396369
ashley simpson <ashley.simpson@mail.mcgill.ca>
2013-11-25 01:35:15 -0500

    Created ReadyTimerListenerTest.java

---
+++ tests/tests/ReadyTimerListenerTest.java
@@ -0,0 +1,17 @@
+/** 
+ * @author Ashley Simpson
+ * @version 2013/11/14
+ * ReadyTimerListener Testing Class
+ */
+
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import startscreen.ReadyTimerListener;
+import javax.swing.JLabel;
+
+public class ReadyTimerListenerTest {
+
+}
