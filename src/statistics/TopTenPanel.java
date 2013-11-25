cc6c64beffa0948bceac3da55d0e37ef3f7c3d07
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-11-24 20:08:11 -0500

    Skeleton from Top Ten Panel

--- src/statistics/TopTenPanel.java
+++ src/statistics/TopTenPanel.java
@@ -1,5 +1,27 @@
 package statistics;
 
-public class TopTenPanel {
+import javax.swing.*;
+
+import java.awt.*;
+import java.sql.*;
+
+
+public class TopTenPanel extends JPanel{
+	
+	
+	public TopTenPanel(Connection conn){
+		this.conn = conn;
+		initComponents();
+		initLayout();	
+	}
+	
+	private void initComponents(){
+		
+		
+	}
+	
+	private void initLayout(){
+		
+	}
 
 }
