c1eb6001c311b2ba87d78e084803671dacb04061
ashley simpson <ashley.simpson@mail.mcgill.ca>
2013-11-25 02:12:05 -0500

    Finished PlayerTest.java tests

--- tests/tests/PlayerTest.java
+++ tests/tests/PlayerTest.java
@@ -12,6 +12,25 @@ import org.junit.Test;
 import user.Player;
 
 public class PlayerTest {
-	 
+	private Player testPlayer;
+	private int playerNum = 1;
+	private String testGetUsername = "anonymous";
+	private int testGetPlayerNum = 1;
+	private String testSetUsername = "testname";
 	
+	@Test
+	public void testGetters() {
+		testPlayer = new Player(playerNum);
+		
+		assertEquals(testGetUsername, testPlayer.getUserName());
+		assertEquals(testGetPlayerNum, testPlayer.getPlayerNumber());
+	}
+	
+	@Test
+	public void testSetters() {
+		testPlayer = new Player(playerNum);
+		
+		testPlayer.setUserName(testSetUsername);
+		assertEquals(testSetUsername, testPlayer.getUserName());
+	}
 }
