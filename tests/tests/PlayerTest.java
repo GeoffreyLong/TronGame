e90def751fd4d37997f60842fd379b613866a0c9
ashley simpson <ashley.simpson@mail.mcgill.ca>
2013-11-25 02:06:47 -0500

    Added previous class commit to the project

---
+++ tests/tests/PlayerTest.java
@@ -0,0 +1,17 @@
+/** 
+ * @author Ashley Simpson
+ * @version 2013/11/25
+ * Player Testing Class
+ */
+
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import user.Player;
+
+public class PlayerTest {
+	 
+	
+}
