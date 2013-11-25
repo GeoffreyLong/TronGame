4d37ebd3e567c637a347294d1067c659c1830503
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-11-24 20:15:40 -0500

    Functionality to make JTable added

--- src/statistics/TopTenPanel.java
+++ src/statistics/TopTenPanel.java
@@ -24,6 +24,33 @@ public class TopTenPanel extends JPanel{
           	ResultSet result = stmt.executeQuery("SELECT @rank := @rank + 1 AS Rank, userName, totalScore, numberWins, numberLosses FROM authentication.allStats ORDER BY totalScore DESC LIMIT 10");
             	ResultSetMetaData md = result.getMetaData();
 
+
+		Object[] columns = {"Rank", "Username", "Total Score", "Number of Wins", "Number of Losses"};
+
+           	Object[][] data = new Object[100][10];
+            
+            	int i = 0;
+            
+            	while(result.next()){
+            	
+            		for(int j = 0 ; j < 5; j++){
+            		
+            			data[i][j] = result.getString(j + 1);
+            		}
+            	
+            		i ++;
+            	}
+
+            	table = new JTable(data, columns);
+            	table.setFillsViewportHeight(true);
+  
+        	}  
+
+        	catch(SQLException sqle){  
+            		System.out.println(sqle);  
+            		sqle.printStackTrace();  
+        	}  
+
 	}
 	
 	private void initLayout(){
