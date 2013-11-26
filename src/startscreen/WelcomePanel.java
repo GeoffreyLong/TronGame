99344f87c274c76611e8aa30423ae55f66c48a57
AZIZ HANNA <aziz.hanna@mail.mcgill.ca>
2013-11-25 20:52:42 -0500

    Color scheme added, Font adjusted

--- src/startscreen/WelcomePanel.java
+++ src/startscreen/WelcomePanel.java
@@ -8,20 +8,25 @@ import javax.swing.JPanel;
 
 import start.Frame;
 import start.Main;
+import java.awt.Font;
 
 public class WelcomePanel extends JPanel {
 	private JLabel welcome;
 	
 	public WelcomePanel(){
+		setBackground(Color.DARK_GRAY);
 		initComponents();
 		initLayout();
 	}	
 	private void initComponents(){
-		welcome = new JLabel("Welcome to Tron");
+		welcome = new JLabel("Welcome to Tron!!!!");
+		welcome.setFont(new Font("Lucida Console", Font.BOLD, 64));
+		welcome.setForeground(Color.ORANGE);
+		welcome.setBackground(Color.DARK_GRAY);
 		welcome.setBounds(start.Frame.getXCenter()-100, 100, 200, 30);
 	}
 	private void initLayout(){
-		setBounds(-1,0,Frame.getXSize(),Frame.getYSize()/4);
+		setBounds(-1,0,Frame.getXSize(),Frame.getYSize()/6);
 		setBorder(BorderFactory.createLineBorder(Color.black));
 		
 		add(welcome);
