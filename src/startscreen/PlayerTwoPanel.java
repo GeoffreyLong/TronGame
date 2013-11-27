06aca3def3a7c0d9d35a3f50c2789528c72bbec3
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-27 01:30:46 -0500

    Fixed statistics visibility

--- src/startscreen/PlayerTwoPanel.java
+++ src/startscreen/PlayerTwoPanel.java
@@ -31,7 +31,7 @@ public class PlayerTwoPanel extends PlayerPanel{
 		setLabel(player.getUserName());
 		login.setVisible(false);
 		createAccount.setVisible(false);
-		playerStats.setVisible(false);
+		playerStats.setVisible(true);
 		playerHistory.setVisible(true);
 		logout.setVisible(true);
 	}
@@ -40,7 +40,7 @@ public class PlayerTwoPanel extends PlayerPanel{
 		setLabel(player.getUserName());
 		login.setVisible(false);
 		createAccount.setVisible(false);
-		playerStats.setVisible(false);
+		playerStats.setVisible(true);
 		playerHistory.setVisible(true);
 		logout.setVisible(true);
 	}
