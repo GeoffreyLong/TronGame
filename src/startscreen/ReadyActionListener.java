c999d97132c506f74e851b8b0ba0532aeea81307
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-28 17:14:36 -0500

    Refactor code
    Refactored the code so that the logic is no longer in the constructor.

--- src/startscreen/ReadyActionListener.java
+++ src/startscreen/ReadyActionListener.java
@@ -9,8 +9,17 @@ import javax.swing.Timer;
 
 public class ReadyActionListener {
 	public static Timer timer;
+	private PlayerPanel paneOne;
+	private PlayerPanel paneTwo;
 
 	public ReadyActionListener(PlayerPanel paneOne, PlayerPanel paneTwo){
+		this.paneOne = paneOne;
+		this.paneTwo = paneTwo;
+		
+		keyBind();
+		setTimer();
+	}
+	private void keyBind(){
 		InputMap imOne = paneOne.inputMap;
 		ActionMap amOne = paneOne.getActionMap();
 		
@@ -22,7 +31,8 @@ public class ReadyActionListener {
 		
 		imTwo.put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "up");
 		amTwo.put("up", new ReadyAction(paneTwo.playerStatus));
-		
+	}
+	private void setTimer(){
 		timer = new Timer(1000, new ReadyTimerListener(paneOne.playerStatus, paneTwo.playerStatus));
 		timer.start();
 	}
