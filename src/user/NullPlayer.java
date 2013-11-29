7be8e8b32a202ccb0da8ebb18ea2edd3c52cefbe
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-29 14:15:16 -0500

    Update Javadocs

--- src/user/NullPlayer.java
+++ src/user/NullPlayer.java
@@ -1,5 +1,10 @@
 package user;
 
+/**
+ * @author Geoffrey Long
+ * 
+ * Provides a way to easily differentiate between logged in and not logged in players
+ */
 public class NullPlayer extends Player{
 
 	public NullPlayer(int playerNumber) {
--- src/user/Player.java
+++ src/user/Player.java
@@ -1,19 +1,43 @@
 package user;
 
+/**
+ * @author Geoffrey Long
+ * 
+ * Provides an object for storing user information such as username, and player number
+ */
 public class Player {
 	private int playerNumber;
 	private String userName;
 	
+	/**
+	 * Constructor to start the player
+	 * @param playerNumber  The number of the player, either 1 or 2
+	 */
 	public Player(int playerNumber){
 		this.userName = "anonymous";
 		this.playerNumber = playerNumber;
 	}
+	
+	/**
+	 * Set the username of the player
+	 * @param userName  The player's username
+	 */
 	public void setUserName(String userName){
 		this.userName = userName;
 	}	
+	
+	/**
+	 * Return the player's username
+	 * @return userName  The player's username
+	 */
 	public final String getUserName(){
 		return this.userName;
 	}
+	
+	/**
+	 * Return the number of the player, either 1 or 2
+	 * @return playerNumber  The number of the player
+	 */
 	public int getPlayerNumber(){
 		return this.playerNumber;
 	}
