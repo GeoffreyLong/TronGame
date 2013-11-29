b0d05ab5e00261ad42bd996196d8762c4a53a1de
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-29 14:38:50 -0500

    Update Javadocs

--- src/game/GameSetup.java
+++ src/game/GameSetup.java
@@ -8,15 +8,18 @@ import user.Player;
  * @author Geoffrey Long
  * 
  * Provides a temporary storage medium to interface between the SetupPanel, 
- * the MapChooser, the GameMaster, and the MapPanel.  The information that 
- * this class stores includes the map, the player colors, and the game difficulty.  
- * The information is set via the user's actions on the SetupPanel and 
- * MapChooser.  It is piped to the MapPanel when the game starts where 
- * it acts on the MapPanel to alter the appearance.  It is also used 
- * in GameMaster to set the refresh rate and therefore the speed of the 
- * cycle.
+ * the MapChooser, the GameMaster, and the MapPanel.  
  */
 public class GameSetup {
+	/*
+	 * The information that 
+	 * this class stores includes the map, the player colors, and the game difficulty.  
+	 * The information is set via the user's actions on the SetupPanel and 
+	 * MapChooser.  It is piped to the MapPanel when the game starts where 
+	 * it acts on the MapPanel to alter the appearance.  It is also used 
+	 * in GameMaster to set the refresh rate and therefore the speed of the 
+	 * cycle.
+	 */
 	Player playerOne;
 	Player playerTwo;
 	private int gameDifficulty = 5;
@@ -35,9 +38,18 @@ public class GameSetup {
 		this.playerTwo = playerTwo;
 	}
 	
+	/**
+	 * Get the MapHandler
+	 * @return mapHandle  An instance of the MapHandler class
+	 */
 	public MapHandler getMapHandler(){
 		return mapHandle;
 	}
+	
+	/**
+	 * Return the game difficulty
+	 * @return gameDifficulty  The difficulty (speed of the cycle movement)
+	 */
 	public int getGameDifficulty(){
 		return this.gameDifficulty;
 	}
@@ -53,9 +65,18 @@ public class GameSetup {
 	public void decrementGameDifficulty(){
 		gameDifficulty--;
 	}
+	
+	/**
+	 * Get the current Map from mapHandle
+	 * @return map  A Map object
+	 */
 	public Map getMap(){
 		return mapHandle.getMap();
 	}
+	
+	/**
+	 * Reset the map, reset based on the current map selection.
+	 */
 	public void resetMap(){
 		switch(mapHandle.getMap().getMapNumber()){
 		case 0:
@@ -72,12 +93,28 @@ public class GameSetup {
 			break;
 		}
 	}
+	
+	/**
+	 * Set the color of the player one cycle.
+	 * @param color  A new Color
+	 */
 	public void setPOneColor(Color color){
 		colorPOne = color;
 	}
+	
+	/**
+	 * Set the color of the player two cycle.
+	 * @param color  A new Color
+	 */
 	public void setPTwoColor(Color color){
 		colorPTwo = color;
 	}
+	
+	/**
+	 * Get the color of the cycle
+	 * @param playerNum  The number of the player
+	 * @return The color of the cycle corresponding to playerNum
+	 */
 	public Color getPlayerColor(int playerNum){
 		if (playerNum == 1){
 			return this.colorPOne;
@@ -86,9 +123,18 @@ public class GameSetup {
 			return this.colorPTwo;
 		}
 	}
-	public void setIncrement(int i){
-		increment = i;
+	
+	/**
+	 * Set the increment
+	 * @param increment  The number of pixels from which every map size and cycle travel distance is determined  
+	 */
+	public void setIncrement(int increment){
+		this.increment = increment;
 	}
+	/**
+	 * Get the increment
+	 * @return increment  The number of pixels from which every map size and cycle travel distance is determined  
+	 */
 	public int getIncrement(){
 		return increment;
 	}
