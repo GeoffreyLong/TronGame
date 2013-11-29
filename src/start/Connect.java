4d05398f97e72533f4c2211eab807c9dda1e57f5
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-11-29 02:46:34 -0500

    Add Javadoc for class and added error message pop up when connection fails

--- src/start/Connect.java
+++ src/start/Connect.java
@@ -2,9 +2,36 @@ package start;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
  
+
+/*
+ * 
+ * @author Rishabh Tandon
+ * @version v1.0
+ * 
+ * 
+ * This class is responsible for connecting to the database.
+ * It provides a static method Connect() which helps with this and can be called from any class to connect to the database.
+ * 
+ */
  
 public class Connect{
+	
+	/*
+	 * 
+	 * This method is used to connect to the database from any other class.
+	 * Method uses the Oracle MySQL J Connector as driver provided by Oracle to connect to the database hosted on Amazon Web Services 
+	 * Ubuntu 12.04 LTE Server. 
+	 * 
+	 * @param none
+	 * 
+	 * @return Connection This is the connection to the database
+	 * 
+	 * 
+	 */
    
    public static Connection connect(){
     	
@@ -18,7 +45,8 @@ public class Connect{
     	}
     	
     	catch(Exception e){
-    	
+    		JFrame frame = new JFrame("Error");
+			JOptionPane.showMessageDialog(frame, "Could not connect to the database. Please check your connection");
     		
     	}
     	
