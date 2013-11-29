17d97dfd669093d6086ed71a2d79a41492adafa9
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-11-29 00:57:16 -0500

    Javadocs for the class

--- src/start/DatabaseCalls.java
+++ src/start/DatabaseCalls.java
@@ -9,10 +9,35 @@ import java.sql.Statement;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+
+/*
+ * 
+ * @author Rishabh Tandon
+ * @version v1.0
+ * 
+ * This class is responsible for all of the calls that are made to the database.
+ * The constructor initializes the connection to the database which is then used in the methods to create a statement. The
+ * statement then runs a query or an update on the database which in our case is hosted on the Amazon Web Services on a Ubuntu Server 12.04 LTE.
+ * The LAMP server consists of the database authentication which has 3 tables: login, allStats and playerHistory and the information from them
+ * is extracted as per required.
+ * 
+ */
+
+
+
 public class DatabaseCalls {
 	
 	private String driver = "com.mysql.jdbc.Driver";
 	private Connection conn = null;
+	
+	
+	/*
+	 * 
+	 * Constructor for class DatabaseCalls. Initializes the connection to the database.
+	 * @param none
+	 * 
+	 */
+	
 	
 	public DatabaseCalls(){
 		
@@ -28,6 +53,25 @@ public class DatabaseCalls {
 		
 		}
 	}
+	
+	
+	/*
+	 * 
+	 * This method creates an account in the database by running an update statement on the login and allStats table.
+	 * The method uses Connection 'conn' to create a statement which runs the update statement on the tables. The statements use
+	 * specific MySQL statements to update the tables but follow the same convention INSERT INTO table (.......) VALUES (.......).
+	 * The account is created only if the username is unique. If that's not the case the method gives a pop up telling the user
+	 * that the username already exists.
+	 * 
+	 *  @param username The desired username of the player. Should be unique
+	 *  @param password The desired password for the player. Must contain at least 1 uppercase, 1 lowercase, 1 number and 1 non-alphanumeric 
+	 *  character and be at least 8 characters long
+	 *  
+	 *  @return boolean True if the update was successful and false otherwise
+	 *  
+	 * 
+	 */
+	
 	
 	public boolean createUser(String userName, String password){
 
@@ -56,6 +100,22 @@ public class DatabaseCalls {
 		
 		
 	}
+	
+	
+	/*
+	 * 
+	 * This method checks if the user records exists in the table login. 
+	 * This information is used to proceed further with the game. The method uses the COnnection 'conn' to create a statement which runs 
+	 * a query on the table login. The MySQL statement selects the row where the username equals the input and then checks if it is same 
+	 * as the inputted password. If that's the case, the user can proceed or else method gives a pop up to check the password.
+	 * 
+	 * @param username The username of the player
+	 * @param password The password of the player
+	 * 
+	 * @return boolean True if the username-password combination is correct and false otherwise
+	 * 
+	 * 
+	 */
 	
 	
 	public boolean login(String userName, String password){
@@ -89,6 +149,24 @@ public class DatabaseCalls {
 		
 		
 	}
+	
+	/*
+	 * 
+	 * This method pushes the statistics to the database. 
+	 * The method takes as input both usernames of the logged in players and their respective scores and stores them in the tables allStats
+	 * and playerHistory in database authentication. The method uses Connection 'conn' to create a statement which is then used to run an update 
+	 * or a query on the table. A bunch of statements is used for two players and their scores. If an account does not exist in playerHistory
+	 * already, it is created using the INSERT statement. For this first a check statement is run and then the row is created in the table.
+	 * 
+	 * @param username1 The username of the first player as a String
+	 * @param score1 The score of the first player as int
+	 * @param username2 The username of the second player as a String
+	 * @param score2 The score of the second player as int
+	 * 
+	 * @return boolean True if the push is successful, and false otherwise
+	 * 
+	 * 
+	 */
 	
 	public boolean pushStatistics(String userName1, int score1, String userName2, int score2){
 		
