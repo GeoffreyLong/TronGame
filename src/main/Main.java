7c8d13d4d86e5e57ef5b85c3a9ced3fb93f3ad72
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-29 18:48:09 -0500

    Update javadocs, add comments

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,3 @@
-/**
- * @author Geoffrey Long
- * 
- * This class serves to start the program.  The primary function is to 
- * instantiate the frame.  This is surrounded by a try-catch to ensure 
- * that no fatal errors arise if the frame cannot be initialized.
- */
 package main;
 
 import javax.swing.UIManager;
@@ -12,6 +5,12 @@ import javax.swing.UIManager;
 import user.NullPlayer;
 import user.Player;
 
+/**
+ * @author Geoffrey Long
+ * 
+ * This class provides the start of the program, it serves to initialize 
+ * the Frame with a MainMenu as well as the player objects.
+ */
 public class Main {
 	public static Player playerOne;
 	public static Player playerTwo;
@@ -21,17 +20,28 @@ public class Main {
 	 * The main method will start the program by instantiating the GUI 
 	 * and the players
 	 * @param args
-	 * @throws java.lang.Exception if the GUI cannot be instantiated
 	 */
 	public static void main (String[] args){
+		//These Player objects are actualy public static, 
+		//they are updated throughout the game
 		playerOne = new NullPlayer(1);
 		playerTwo = new NullPlayer(2);
+		
 		frame = new Frame();
 		initializeLookAndFeel();
 		start();
 	}
 	
+	/**
+	 * This method will initialize the look and feel of the GUI
+	 */
 	private static void initializeLookAndFeel(){
+		/*
+		 * The look and feel will regulate how the individual styling looks.  
+		 * This tries two UI managers, GTK and Nimbus.  If neither of these 
+		 * are able to be used then the default UI manager is used.  This 
+		 * isn't really considered a problem as it is only for styling.
+		 */
 		try {
 			UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
 		} catch (Exception e) {
@@ -43,8 +53,13 @@ public class Main {
 		}
 	}
 	
+	/**
+	 * Start the main menu
+	 */
 	private static void start(){
 		new FrameDriver(frame);
+		
+		//This will initialize most of the panels of FrameDriver
 		FrameDriver.init();
 		FrameDriver.mainMenu();
 	}
