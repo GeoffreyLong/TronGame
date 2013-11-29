2b6bbdf0ed173960bd5ff07cfbcc6920b8d96922
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2013-11-29 14:55:59 -0500

    Update Javadocs

--- src/game/WinCondition.java
+++ src/game/WinCondition.java
@@ -1,5 +1,10 @@
 package game;
 
+/**
+ * @author Geoffrey Long
+ * 
+ * An enum to provide a typesafe object to handle the various win conditions
+ */
 public enum WinCondition {
 	TIE, PONE_WIN, PTWO_WIN;
 }
